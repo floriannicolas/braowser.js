6835c43c6106273216f792a71e926818669ad31d
Florian Nicolas <florian@iMac-de-Florian.local>
2016-06-10 14:58:44 +0200

    Version 1.0.0
    First release

--- README.md
+++ README.md
@@ -1,1 +1,95 @@
-# Braowser
+# braowser.js
+
+### A very simple javascript plugin for browser detection who just add related classes to html element.
+
+braowser.js can detect : 
+
+* Operating system
+* Browser
+* Browser version
+* Screen resolution
+
+
+## Examples : 
+
+```html
+<html class="mac firefox v-48 retina">
+```
+```html
+<html class="windows ie v-9">
+```
+
+## How to use it
+
+To use braowser.js just add this following line in your head element (braowser.js doesn't need jquery or something else to work).
+
+```html
+<script type="text/javascript" src="/path/to/braowser.min.js"></script>
+```
+
+That's it!
+
+
+
+
+## Documentation
+
+#### Operating systems detected :
+* mac 
+* windows
+* linux
+ 
+#### Browsers detected :
+* chrome
+* safari 
+* firefox
+* edge
+* ie
+* opera
+
+#### Screen resolution :
+
+if devicePixelRatio >= 2 braowser.js add "retina" class to html element.
+
+
+### Examples of usecases in CSS
+
+```css
+html.ie.v-8{
+    border: 1px solid #999;
+}
+
+html.safari,
+html.chrome{
+	-webkit-font-smoothing: subpixel-antialiased;
+}
+
+html.retina{
+	background-image:url(image@2x.png);
+}
+```
+
+### Examples of usecases in Javascript
+
+```javascript
+// Only javascript
+if((' ' + document.documentElement.className + ' ').indexOf('ie v-8') > -1){
+	// Do something if browser is IE8
+}
+
+// with jQuery or Zepto
+if($('html').hasClass('ie v-8')){
+	// Do something if browser is IE8
+}
+```
+
+
+## Contribute
+
+We're always looking for:
+
+* Bug reports, especially those for aspects with a reduced test case
+* Pull requests for features, spelling errors, clarifications, etc.
+* Ideas for enhancements
+
+
---
+++ braowser.js
@@ -0,0 +1,79 @@
+/*!
+ Braowser - 1.0.0
+ Copyright © 2016 Florian Nicolas
+ Licensed under the MIT license.
+ https://github.com/ticlekiwi/braowser
+ !*/
+
+var classes = '';
+var os = navigator.platform;
+var vendor = navigator.vendor;
+var user_agent = navigator.userAgent;
+var version = null;
+var name = '';
+
+/* Get operating system
+ ----------------------------------------------------------------------------------------*/
+
+if (/Mac/.test(os)) {
+    classes += " mac";
+} else if (/Linux/.test(os)) {
+    classes += " linux";
+
+} else if (/Win/.test(os)) {
+    classes += " windows";
+}
+
+/* Get browser
+ ----------------------------------------------------------------------------------------*/
+
+if (/Apple/.test(vendor)) {
+    name = 'Safari';
+    classes += " safari";
+} else if (/Google/.test(vendor)){
+    name = 'Chrome';
+    classes += " chrome";
+} else if (/Firefox/.test(user_agent)) {
+    name = 'Firefox';
+    classes += " firefox";
+} else if (/Edge/.test(user_agent)) {
+    name = 'Edge';
+    classes += " edge";
+} else if (/MSIE/.test(user_agent)) {
+    name = 'Internet Explorer';
+    classes += " ie";
+    version = user_agent.match(/MSIE (\d+(?:\.\d+)+(?:b\d*)?)/);
+} else if (/Gecko|Mozilla/.test(user_agent)) {
+    name = 'Mozilla';
+    version = user_agent.match(/rv:(\d+(?:\.\d+)+)/);
+} else if (window.opera != undefined) {
+    name = 'Opera';
+    classes += " opera";
+}
+if (!version) {
+    var regex = new RegExp(name + '(?:\\s|\\/)(\\d+(?:\\.\\d+)+(?:(?:a|b)\\d*)?)');
+    var result = user_agent.match(regex);
+    version = parseFloat(result[1]);
+}
+if (name == 'Mozilla' && version >= 6 && version <= 12) {
+    classes += " ie";
+}
+
+classes += " v-" + version;
+
+/* Check if is retina
+ ----------------------------------------------------------------------------------------*/
+
+if (window.devicePixelRatio >= 2) {
+    classes += " retina";
+} 
+
+/* Add Classes
+ ----------------------------------------------------------------------------------------*/
+
+document.documentElement.className += classes;
+console.log('braowser.js : ' + classes);
+
+
+
+
